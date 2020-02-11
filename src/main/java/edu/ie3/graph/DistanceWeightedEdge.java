36a4f6c8b2a170d30f5fe74bfb9e096cc9921ad6
Christian Mahr <cmahr93@gmail.com>
2020-02-11 11:43:31 +0100

    Removed default constructor and call of DEFAULT_EDGE_WEIGHT.

--- src/main/java/edu/ie3/graph/DistanceWeightedEdge.java
+++ src/main/java/edu/ie3/graph/DistanceWeightedEdge.java
@@ -11,7 +11,6 @@ import javax.measure.Quantity;
 import javax.measure.Unit;
 import javax.measure.quantity.Length;
 import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.SimpleWeightedGraph;
 import tec.uom.se.quantity.Quantities;
 
 /**
@@ -23,18 +22,16 @@ public class DistanceWeightedEdge extends DefaultWeightedEdge {
 
   private static final long serialVersionUID = -1679382970341818555L;
 
-  public static final Unit<Length> DEFAULT_UNIT = METRE;
+  protected static final Unit<Length> DEFAULT_UNIT = METRE;
 
-  private double weight = SimpleWeightedGraph.DEFAULT_EDGE_WEIGHT;
-
-  public DistanceWeightedEdge() {}
+  private double distance;
 
   public DistanceWeightedEdge(Quantity<Length> weightQuantity) {
-    this.weight = weightQuantity.to(DEFAULT_UNIT).getValue().doubleValue();
+    this.distance = weightQuantity.to(DEFAULT_UNIT).getValue().doubleValue();
   }
 
   /** Returns the weightQuantity of the edge as {@link Quantity}. */
   protected Quantity<Length> getWeightQuantity() {
-    return Quantities.getQuantity(weight, DEFAULT_UNIT);
+    return Quantities.getQuantity(distance, DEFAULT_UNIT);
   }
 }
