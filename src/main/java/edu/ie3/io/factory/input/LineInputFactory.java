4b5163494d6fd42ca10a513b1c8f5c70cbd6ee81
Sebastian Peter <14994800+sebastian-peter@users.noreply.github.com>
2020-02-19 17:26:16 +0100

    format

--- src/main/java/edu/ie3/io/factory/input/LineInputFactory.java
+++ src/main/java/edu/ie3/io/factory/input/LineInputFactory.java
@@ -1,27 +1,45 @@
+/*
+ * © 2020. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+*/
 package edu.ie3.io.factory.input;
 
 import edu.ie3.models.OperationTime;
 import edu.ie3.models.input.NodeInput;
 import edu.ie3.models.input.OperatorInput;
 import edu.ie3.models.input.connector.LineInput;
-
 import java.util.UUID;
 
-public class LineInputFactory extends ConnectorInputEntityFactory<LineInput, ConnectorInputEntityData> {
+public class LineInputFactory
+    extends ConnectorInputEntityFactory<LineInput, ConnectorInputEntityData> {
 
-    @Override
-    protected String[] getAdditionalFields() {
-        return new String[0];
-    }
+  @Override
+  protected String[] getAdditionalFields() {
+    return new String[0];
+  }
 
-    @Override
-    protected LineInput buildModel(ConnectorInputEntityData data, UUID uuid, String id, NodeInput nodeA, NodeInput nodeB, int parallelDevices, OperatorInput operatorInput, OperationTime operationTime) {
-        return null;
-    }
+  @Override
+  protected LineInput buildModel(
+      ConnectorInputEntityData data,
+      UUID uuid,
+      String id,
+      NodeInput nodeA,
+      NodeInput nodeB,
+      int parallelDevices,
+      OperatorInput operatorInput,
+      OperationTime operationTime) {
+    return null;
+  }
 
-    @Override
-    protected LineInput buildModel(ConnectorInputEntityData data, UUID uuid, String id, NodeInput nodeA, NodeInput nodeB, int parallelDevices) {
-        return null;
-    }
-
+  @Override
+  protected LineInput buildModel(
+      ConnectorInputEntityData data,
+      UUID uuid,
+      String id,
+      NodeInput nodeA,
+      NodeInput nodeB,
+      int parallelDevices) {
+    return null;
+  }
 }
