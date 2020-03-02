5fa69fb4ea08cafd7822c88a85a955e5d93cb17b
Kittl, Chris <chris.kittl@tu-dortmund.de>
2020-03-02 15:24:54 +0100

    Fixing inheritance issue and formatting

--- src/main/java/edu/ie3/models/timeseries/mapping/HeatAndPowerTimeSeriesMap.java
+++ src/main/java/edu/ie3/models/timeseries/mapping/HeatAndPowerTimeSeriesMap.java
@@ -1,8 +1,12 @@
+/*
+ * © 2020. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+*/
 package edu.ie3.models.timeseries.mapping;
 
 import edu.ie3.models.input.system.SystemParticipantInput;
-
 import java.util.HashMap;
 
-public class HeatAndPowerTimeSeriesMap extends HashMap<SystemParticipantInput, HeatAndPowerTimeSeriesMap> {
-}
+public class HeatAndPowerTimeSeriesMap
+    extends HashMap<SystemParticipantInput, HeatAndPowerTimeSeriesMap> {}
--- src/main/java/edu/ie3/models/timeseries/mapping/PowerTimeSeriesMap.java
+++ src/main/java/edu/ie3/models/timeseries/mapping/PowerTimeSeriesMap.java
@@ -1,9 +1,12 @@
+/*
+ * © 2020. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+*/
 package edu.ie3.models.timeseries.mapping;
 
 import edu.ie3.models.input.system.SystemParticipantInput;
 import edu.ie3.models.timeseries.PowerTimeSeries;
-
 import java.util.HashMap;
 
-public class PowerTimeSeriesMap extends HashMap<SystemParticipantInput, PowerTimeSeries> {
-}
+public class PowerTimeSeriesMap extends HashMap<SystemParticipantInput, PowerTimeSeries> {}
