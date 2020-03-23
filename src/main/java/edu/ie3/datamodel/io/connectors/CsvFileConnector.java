26562cb84b20dd2f96ac7d866169b37ceb7b3d8a
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-03-23 17:57:58 +0100

    documentation in CsvFileConnector

--- src/main/java/edu/ie3/datamodel/io/connectors/CsvFileConnector.java
+++ src/main/java/edu/ie3/datamodel/io/connectors/CsvFileConnector.java
@@ -17,7 +17,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * //ToDo: Class Description test
+ * Provides the connector (here: buffered writer) for specific files to be used by a {@link
+ * edu.ie3.datamodel.io.sink.CsvFileSink}
  *
  * @version 0.1
  * @since 19.03.20
