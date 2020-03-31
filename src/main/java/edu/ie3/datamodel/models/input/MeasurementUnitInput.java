4cb0463db064bcba6bd880399ce646cc91a67832
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-03-31 15:06:26 +0200

    added Nodes interface to corresponding classes

--- src/main/java/edu/ie3/datamodel/models/input/MeasurementUnitInput.java
+++ src/main/java/edu/ie3/datamodel/models/input/MeasurementUnitInput.java
@@ -2,133 +2,146 @@
  * © 2020. TU Dortmund University,
  * Institute of Energy Systems, Energy Efficiency and Energy Economics,
  * Research group Distribution grid planning and operation
-*/
+ */
 package edu.ie3.datamodel.models.input;
 
+import edu.ie3.datamodel.models.Nodes;
 import edu.ie3.datamodel.models.OperationTime;
-import java.util.Objects;
-import java.util.UUID;
 
-/** Model of a measuring unit attached to a certain {@link NodeInput}. */
-public class MeasurementUnitInput extends AssetInput {
-  /** Grid node, the asset is attached to */
-  private final NodeInput node;
+import java.util.*;
 
-  /** True, if the voltage magnitude is measured */
-  private final boolean vMag;
 
-  /** True, if the voltage angle is measured */
-  private final boolean vAng;
+/**
+ * Model of a measuring unit attached to a certain {@link NodeInput}.
+ */
+public class MeasurementUnitInput extends AssetInput implements Nodes {
+    /**
+     * Grid node, the asset is attached to
+     */
+    private final NodeInput node;
 
-  /** True, if the nodal residual active power is measured */
-  private final boolean p;
+    /**
+     * True, if the voltage magnitude is measured
+     */
+    private final boolean vMag;
 
-  /** True, if the reactive power is measured */
-  private final boolean q;
+    /**
+     * True, if the voltage angle is measured
+     */
+    private final boolean vAng;
 
-  /**
-   * Constructor for an operated measurement unit
-   *
-   * @param uuid of the input entity
-   * @param operationTime Time for which the entity is operated
-   * @param operator of the asset
-   * @param id of the asset
-   * @param node Grid node, the asset is attached to
-   * @param vMag True, if the voltage magnitude is measured
-   * @param vAng True, if the voltage angle is measured
-   * @param p True, if the nodal residual active power is measured
-   * @param q True, if the reactive power is measured
-   */
-  public MeasurementUnitInput(
-      UUID uuid,
-      OperationTime operationTime,
-      OperatorInput operator,
-      String id,
-      NodeInput node,
-      boolean vMag,
-      boolean vAng,
-      boolean p,
-      boolean q) {
-    super(uuid, operationTime, operator, id);
-    this.node = node;
-    this.vMag = vMag;
-    this.vAng = vAng;
-    this.p = p;
-    this.q = q;
-  }
+    /**
+     * True, if the nodal residual active power is measured
+     */
+    private final boolean p;
 
-  /**
-   * Constructor for a non-operated measurement unit
-   *
-   * @param uuid of the input entity
-   * @param id of the asset
-   * @param node Grid node, the asset is attached to
-   * @param vMag True, if the voltage magnitude is measured
-   * @param vAng True, if the voltage angle is measured
-   * @param p True, if the nodal residual active power is measured
-   * @param q True, if the reactive power is measured
-   */
-  public MeasurementUnitInput(
-      UUID uuid, String id, NodeInput node, boolean vMag, boolean vAng, boolean p, boolean q) {
-    super(uuid, id);
-    this.node = node;
-    this.vMag = vMag;
-    this.vAng = vAng;
-    this.p = p;
-    this.q = q;
-  }
+    /**
+     * True, if the reactive power is measured
+     */
+    private final boolean q;
 
-  public NodeInput getNode() {
-    return node;
-  }
+    /**
+     * Constructor for an operated measurement unit
+     *
+     * @param uuid          of the input entity
+     * @param operationTime Time for which the entity is operated
+     * @param operator      of the asset
+     * @param id            of the asset
+     * @param node          Grid node, the asset is attached to
+     * @param vMag          True, if the voltage magnitude is measured
+     * @param vAng          True, if the voltage angle is measured
+     * @param p             True, if the nodal residual active power is measured
+     * @param q             True, if the reactive power is measured
+     */
+    public MeasurementUnitInput(UUID uuid,
+                                OperationTime operationTime,
+                                OperatorInput operator,
+                                String id,
+                                NodeInput node,
+                                boolean vMag,
+                                boolean vAng,
+                                boolean p,
+                                boolean q) {
+        super(uuid, operationTime, operator, id);
+        this.node = node;
+        this.vMag = vMag;
+        this.vAng = vAng;
+        this.p = p;
+        this.q = q;
+    }
 
-  public boolean getVMag() {
-    return vMag;
-  }
+    /**
+     * Constructor for a non-operated measurement unit
+     *
+     * @param uuid of the input entity
+     * @param id   of the asset
+     * @param node Grid node, the asset is attached to
+     * @param vMag True, if the voltage magnitude is measured
+     * @param vAng True, if the voltage angle is measured
+     * @param p    True, if the nodal residual active power is measured
+     * @param q    True, if the reactive power is measured
+     */
+    public MeasurementUnitInput(UUID uuid,
+                                String id,
+                                NodeInput node,
+                                boolean vMag,
+                                boolean vAng,
+                                boolean p,
+                                boolean q) {
+        super(uuid, id);
+        this.node = node;
+        this.vMag = vMag;
+        this.vAng = vAng;
+        this.p = p;
+        this.q = q;
+    }
 
-  public boolean getVAng() {
-    return vAng;
-  }
+    public NodeInput getNode() {
+        return node;
+    }
 
-  public boolean getP() {
-    return p;
-  }
+    public boolean getVMag() {
+        return vMag;
+    }
 
-  public boolean getQ() {
-    return q;
-  }
+    public boolean getVAng() {
+        return vAng;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    if (!super.equals(o)) return false;
-    MeasurementUnitInput that = (MeasurementUnitInput) o;
-    return Objects.equals(node, that.node)
-        && Objects.equals(vMag, that.vMag)
-        && Objects.equals(vAng, that.vAng)
-        && Objects.equals(p, that.p)
-        && Objects.equals(q, that.q);
-  }
+    public boolean getP() {
+        return p;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(super.hashCode(), node, vMag, vAng, p, q);
-  }
+    public boolean getQ() {
+        return q;
+    }
 
-  @Override
-  public String toString() {
-    return "MeasurementUnitInput{"
-        + "node="
-        + node
-        + ", vMag="
-        + vMag
-        + ", vAng="
-        + vAng
-        + ", p="
-        + p
-        + ", q="
-        + q
-        + '}';
-  }
+    @Override
+    public boolean equals(Object o) {
+        if(this == o)
+            return true;
+        if(o == null || getClass() != o.getClass())
+            return false;
+        if(!super.equals(o))
+            return false;
+        MeasurementUnitInput that = (MeasurementUnitInput) o;
+        return Objects.equals(node, that.node) && Objects.equals(vMag, that.vMag) && Objects.equals(vAng, that.vAng) &&
+               Objects.equals(p, that.p) && Objects.equals(q, that.q);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), node, vMag, vAng, p, q);
+    }
+
+    @Override
+    public String toString() {
+        return "MeasurementUnitInput{" + "node=" + node + ", vMag=" + vMag + ", vAng=" + vAng + ", p=" + p + ", q=" +
+               q + '}';
+    }
+
+    @Override
+    public List<NodeInput> getNodes() {
+        return Collections.unmodifiableList(Collections.singletonList(node));
+    }
 }
--- src/main/java/edu/ie3/datamodel/models/input/system/SystemParticipantInput.java
+++ src/main/java/edu/ie3/datamodel/models/input/system/SystemParticipantInput.java
@@ -2,92 +2,100 @@
  * © 2020. TU Dortmund University,
  * Institute of Energy Systems, Energy Efficiency and Energy Economics,
  * Research group Distribution grid planning and operation
-*/
+ */
 package edu.ie3.datamodel.models.input.system;
 
+import com.sun.org.apache.xalan.internal.lib.NodeInfo;
+import edu.ie3.datamodel.models.Nodes;
 import edu.ie3.datamodel.models.OperationTime;
 import edu.ie3.datamodel.models.input.AssetInput;
 import edu.ie3.datamodel.models.input.NodeInput;
 import edu.ie3.datamodel.models.input.OperatorInput;
-import java.util.Objects;
-import java.util.UUID;
 
-/** Describes a system asset that is connected to a node */
-public abstract class SystemParticipantInput extends AssetInput {
+import java.util.*;
 
-  /** The node that the asset is connected to */
-  private final NodeInput node;
 
-  /** Description of a reactive power characteristic. For details see further documentation */
-  private final String qCharacteristics;
+/**
+ * Describes a system asset that is connected to a node
+ */
+public abstract class SystemParticipantInput extends AssetInput implements Nodes {
 
-  /**
-   * Constructor for an operated system participant
-   *
-   * @param uuid of the input entity
-   * @param operationTime Time for which the entity is operated
-   * @param operator of the asset
-   * @param id of the asset
-   * @param node that the asset is connected to
-   * @param qCharacteristics Description of a reactive power characteristic
-   */
-  public SystemParticipantInput(
-      UUID uuid,
-      OperationTime operationTime,
-      OperatorInput operator,
-      String id,
-      NodeInput node,
-      String qCharacteristics) {
-    super(uuid, operationTime, operator, id);
-    this.node = node;
-    this.qCharacteristics = qCharacteristics;
-  }
+    /**
+     * The node that the asset is connected to
+     */
+    private final NodeInput node;
 
-  /**
-   * Constructor for a non-operated system participant
-   *
-   * @param uuid of the input entity
-   * @param id of the asset
-   * @param node that the asset is connected to
-   * @param qCharacteristics Description of a reactive power characteristic
-   */
-  public SystemParticipantInput(UUID uuid, String id, NodeInput node, String qCharacteristics) {
-    super(uuid, id);
-    this.node = node;
-    this.qCharacteristics = qCharacteristics;
-  }
+    /**
+     * Description of a reactive power characteristic. For details see further documentation
+     */
+    private final String qCharacteristics;
 
-  public String getqCharacteristics() {
-    return qCharacteristics;
-  }
+    /**
+     * Constructor for an operated system participant
+     *
+     * @param uuid             of the input entity
+     * @param operationTime    Time for which the entity is operated
+     * @param operator         of the asset
+     * @param id               of the asset
+     * @param node             that the asset is connected to
+     * @param qCharacteristics Description of a reactive power characteristic
+     */
+    public SystemParticipantInput(UUID uuid,
+                                  OperationTime operationTime,
+                                  OperatorInput operator,
+                                  String id,
+                                  NodeInput node,
+                                  String qCharacteristics) {
+        super(uuid, operationTime, operator, id);
+        this.node = node;
+        this.qCharacteristics = qCharacteristics;
+    }
 
-  public NodeInput getNode() {
-    return node;
-  }
+    /**
+     * Constructor for a non-operated system participant
+     *
+     * @param uuid             of the input entity
+     * @param id               of the asset
+     * @param node             that the asset is connected to
+     * @param qCharacteristics Description of a reactive power characteristic
+     */
+    public SystemParticipantInput(UUID uuid, String id, NodeInput node, String qCharacteristics) {
+        super(uuid, id);
+        this.node = node;
+        this.qCharacteristics = qCharacteristics;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    if (!super.equals(o)) return false;
-    SystemParticipantInput that = (SystemParticipantInput) o;
-    return Objects.equals(node, that.node)
-        && Objects.equals(qCharacteristics, that.qCharacteristics);
-  }
+    public String getqCharacteristics() {
+        return qCharacteristics;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(super.hashCode(), node, qCharacteristics);
-  }
+    public NodeInput getNode() {
+        return node;
+    }
 
-  @Override
-  public String toString() {
-    return "SystemParticipantInput{"
-        + "node="
-        + node
-        + ", qCharacteristics='"
-        + qCharacteristics
-        + '\''
-        + '}';
-  }
+    public List<NodeInput> getNodes() {
+        return Collections.singletonList(node);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o)
+            return true;
+        if(o == null || getClass() != o.getClass())
+            return false;
+        if(!super.equals(o))
+            return false;
+        SystemParticipantInput that = (SystemParticipantInput) o;
+        return Objects.equals(node, that.node) && Objects.equals(qCharacteristics, that.qCharacteristics);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), node, qCharacteristics);
+    }
+
+    @Override
+    public String toString() {
+        return "SystemParticipantInput{" + "node=" + node + ", qCharacteristics='" + qCharacteristics + '\'' + '}';
+    }
 }
