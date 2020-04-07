cb370f21ec721081400edbde40f2020443bd644a
Kittl, Chris <chris.kittl@tu-dortmund.de>
2020-04-07 15:53:50 +0200

    Enhancing documentation

--- src/main/java/edu/ie3/datamodel/models/input/system/characteristic/EvCharacteristicInput.java
+++ src/main/java/edu/ie3/datamodel/models/input/system/characteristic/EvCharacteristicInput.java
@@ -14,16 +14,17 @@ import javax.measure.quantity.Power;
 /**
  * Represents the charging characteristic in dependency of the current state of charge as a
  * dimensionless multiplier to the rated active power
+ *
+ * @deprecated This model is currently not well worked out. Wait for a more elaborate model.
  */
+@Deprecated
 public class EvCharacteristicInput extends CharacteristicInput<Power, Dimensionless> {
 
-  @Deprecated
   public EvCharacteristicInput(
       SortedSet<CharacteristicCoordinate<Power, Dimensionless>> characteristicCoordinates) {
     super(characteristicCoordinates, "ev", 2);
   }
 
-  @Deprecated
   public EvCharacteristicInput(String input) throws ParsingException {
     super(input, StandardUnits.ACTIVE_POWER_IN, StandardUnits.EV_CHARACTERISTIC, "ev", 2);
   }
