3082f1d39da8a16a8e1cf90226976e1fd152f44f
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-04-14 08:53:58 +0200

    added empty files for CsvRawGridSourceTest + CsvSystemParticipantSourceTest

---
+++ src/test/groovy/edu/ie3/datamodel/io/source/csv/CsvRawGridSourceTest.groovy
@@ -0,0 +1,16 @@
+/*
+ * © 2020. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+ */
+package edu.ie3.datamodel.io.source.csv
+
+
+import spock.lang.Specification
+
+class CsvRawGridSourceTest extends Specification implements CsvTestDataMeta {
+
+// todo
+
+
+}
---
+++ src/test/groovy/edu/ie3/datamodel/io/source/csv/CsvSystemParticipantSourceTest.groovy
@@ -0,0 +1,24 @@
+/*
+ * © 2020. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+ */
+package edu.ie3.datamodel.io.source.csv
+
+
+import edu.ie3.datamodel.io.factory.input.graphics.LineGraphicInputEntityData
+import edu.ie3.datamodel.io.factory.input.graphics.NodeGraphicInputEntityData
+import edu.ie3.datamodel.io.source.RawGridSource
+import edu.ie3.datamodel.models.input.NodeInput
+import edu.ie3.datamodel.models.input.graphics.NodeGraphicInput
+import edu.ie3.test.common.GridTestData as gtd
+import org.locationtech.jts.geom.LineString
+import org.locationtech.jts.geom.Point
+import spock.lang.Specification
+
+class CsvSystemParticipantSourceTest extends Specification implements CsvTestDataMeta {
+
+// todo
+
+
+}
