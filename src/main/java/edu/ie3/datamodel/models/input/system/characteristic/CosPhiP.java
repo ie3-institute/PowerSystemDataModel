048757d60b25a0f1d431f04cd2226df1a7ce62da
Kittl, Chris <chris.kittl@tu-dortmund.de>
2020-04-15 10:26:35 +0200

    Adapting the equals and hash code methods for characteristics

--- src/main/java/edu/ie3/datamodel/models/input/system/characteristic/CosPhiP.java
+++ src/main/java/edu/ie3/datamodel/models/input/system/characteristic/CosPhiP.java
@@ -28,6 +28,16 @@ public class CosPhiP extends ReactivePowerCharacteristic {
   }
 
   @Override
+  public boolean equals(Object o) {
+    return super.equals(o);
+  }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
+
+  @Override
   public String toString() {
     return "CosPhiP{" + "points=" + points + '}';
   }
