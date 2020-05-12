709cbbbc7fab6095ab8ecbd9da364d63d22da2bf
Mia <mia.krause004@stud.fh-dortmund.de>
2020-05-12 15:36:38 +0200

    Added documentation

--- src/test/groovy/edu/ie3/datamodel/io/source/influxdb/InfluxDbWeatherSourceTest.groovy
+++ src/test/groovy/edu/ie3/datamodel/io/source/influxdb/InfluxDbWeatherSourceTest.groovy
@@ -32,6 +32,8 @@ class InfluxDbWeatherSourceTest extends Specification implements WeatherSourceTe
 	InfluxDbWeatherSource source
 
 	def setupSpec() {
+		// Copy import file into docker and then import it via influx CLI
+		// more information on file format and usage here: https://docs.influxdata.com/influxdb/v1.7/tools/shell/#import-data-from-a-file-with-import
 		MountableFile influxWeatherImportFile = MountableFile.forClasspathResource("/testcontainersFiles/influxDb/weather.txt");
 		influxDbContainer.copyFileToContainer(influxWeatherImportFile, "/home/weather.txt")
 		def execResult = influxDbContainer.execInContainer("influx", "-import", "-path=/home/weather.txt", "-precision=ms")
