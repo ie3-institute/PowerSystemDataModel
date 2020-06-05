2e2e6d7a868ccdb7dee7d88edc35bea050856d75
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-06-05 12:06:03 +0200

    added tests for copy builder

---
+++ src/test/groovy/edu/ie3/datamodel/models/input/connector/SwitchInputTest.groovy
@@ -0,0 +1,32 @@
+/*
+ * © 2020. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+ */
+package edu.ie3.datamodel.models.input.connector
+
+import edu.ie3.datamodel.models.input.OperatorInput
+import edu.ie3.test.common.GridTestData
+import spock.lang.Specification
+
+
+class SwitchInputTest extends Specification {
+
+	def "A SwitchInput copy method should work as expected"() {
+		given:
+		def switchInput = GridTestData.switchAtoB
+
+		when:
+		def alteredUnit = switchInput.copy().id("switch_A_C").operator(OperatorInput.NO_OPERATOR_ASSIGNED)
+				.closed(false).build()
+
+		then:
+		alteredUnit.with {
+			assert uuid == switchInput.uuid
+			assert operationTime == switchInput.operationTime
+			assert operator == OperatorInput.NO_OPERATOR_ASSIGNED
+			assert id == "switch_A_C"
+			assert !closed
+		}
+	}
+}
---
+++ src/test/groovy/edu/ie3/datamodel/models/input/connector/Transformer3WInputTest.groovy
@@ -0,0 +1,36 @@
+/*
+ * © 2020. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+ */
+package edu.ie3.datamodel.models.input.connector
+
+import edu.ie3.test.common.GridTestData
+import spock.lang.Specification
+
+
+class Transformer3WInputTest extends Specification {
+
+	def "A Transformer3WInput copy method should work as expected"() {
+		given:
+		def trafo3w = GridTestData.transformerAtoBtoC
+
+		when:
+		def alteredUnit = trafo3w.copy().id("trafo3w").nodeA(GridTestData.nodeC).nodeB(GridTestData.nodeD)
+				.nodeC(GridTestData.nodeB).type(GridTestData.transformerTypeAtoBtoC).tapPos(10).autoTap(false).build()
+
+		then:
+		alteredUnit.with {
+			assert uuid == trafo3w.uuid
+			assert operationTime == trafo3w.operationTime
+			assert operator == GridTestData.profBroccoli
+			assert id == "trafo3w"
+			assert nodeA == GridTestData.nodeC
+			assert nodeB == GridTestData.nodeD
+			assert nodeC == GridTestData.nodeB
+			assert type == GridTestData.transformerTypeAtoBtoC
+			assert tapPos == 10
+			assert !autoTap
+		}
+	}
+}
