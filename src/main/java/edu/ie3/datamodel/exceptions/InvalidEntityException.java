d3eb249c9ed24484170bba3211316be9ba95b8e1
Kittl, Chris <chris.kittl@tu-dortmund.de>
2020-09-02 09:11:49 +0200

    Adding and adapting exceptions

--- src/main/java/edu/ie3/datamodel/exceptions/InvalidEntityException.java
+++ src/main/java/edu/ie3/datamodel/exceptions/InvalidEntityException.java
@@ -8,8 +8,13 @@ package edu.ie3.datamodel.exceptions;
 import edu.ie3.datamodel.models.UniqueEntity;
 
 /** Is thrown, when a checked entity is illegal and thus not usable */
-public class InvalidEntityException extends RuntimeException {
+public class InvalidEntityException extends ValidationException {
   public InvalidEntityException(String faultDescription, UniqueEntity invalidEntity) {
     super("Entity is invalid because of: " + faultDescription + " [" + invalidEntity + "]");
+  }
+
+  public InvalidEntityException(
+      String faultDescription, Throwable cause, UniqueEntity invalidEntity) {
+    super("Entity is invalid because of: " + faultDescription + " [" + invalidEntity + "]", cause);
   }
 }
