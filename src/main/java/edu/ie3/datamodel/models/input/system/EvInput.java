50a19e617313d0f475f24fad4bde288ed5a96da0
Vasilios Zachopoulos <47082864+baszach@users.noreply.github.com>
2020-09-24 14:51:24 +0200

    Update src/main/java/edu/ie3/datamodel/models/input/system/EvInput.java
    Co-authored-by: johanneshiry <johannes.hiry@tu-dortmund.de>

--- src/main/java/edu/ie3/datamodel/models/input/system/EvInput.java
+++ src/main/java/edu/ie3/datamodel/models/input/system/EvInput.java
@@ -89,9 +89,8 @@ public class EvInput extends SystemParticipantInput implements HasType {
     return "EvInput{"
         + "uuid="
         + getUuid()
-        + ", id='"
+        + ", id="
         + getId()
-        + '\''
         + ", operator="
         + getOperator().getUuid()
         + ", operationTime="
