d3ded56bd8781751854428559f23e2584cc00640
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-10-12 21:23:45 +0200

    added IrradiationValueTest

---
+++ src/test/groovy/edu/ie3/datamodel/models/value/IrradiationValueTest.groovy
@@ -0,0 +1,26 @@
+package edu.ie3.datamodel.models.value
+
+import edu.ie3.datamodel.models.StandardUnits
+import edu.ie3.util.quantities.PowerSystemUnits
+import spock.lang.Specification
+import tech.units.indriya.quantity.Quantities
+
+
+class IrradiationValueTest extends Specification {
+
+    def "An Irradiation's equal method should work as expected on null and provided quantities"() {
+        expect:
+        (iVal1 == iVal2) == res
+
+        where:
+        iVal1                                                                                                | iVal2                                                                                || res
+        new IrradiationValue(null, null)                                                                     | new IrradiationValue(null, null)                                                     || true
+        new IrradiationValue(Quantities.getQuantity(10d, StandardUnits.IRRADIATION), null)                   | new IrradiationValue(Quantities.getQuantity(10d, StandardUnits.IRRADIATION), null)   || true
+        new IrradiationValue(Quantities.getQuantity(10d, StandardUnits.IRRADIATION), null)                   | new IrradiationValue(null, null)                                                     || false
+        null                                                                                                 | new IrradiationValue(null, null)                                                     || false
+        null                                                                                                 | null                                                                                 || true
+        new IrradiationValue(Quantities.getQuantity(10.23d, StandardUnits.IRRADIATION), null)                | new IrradiationValue(Quantities.getQuantity(10.23, StandardUnits.IRRADIATION), null) || false
+        new IrradiationValue(Quantities.getQuantity(10230, PowerSystemUnits.WATTHOUR_PER_SQUAREMETRE), null) | new IrradiationValue(Quantities.getQuantity(10.23, StandardUnits.IRRADIATION), null) || false
+    }
+
+}
