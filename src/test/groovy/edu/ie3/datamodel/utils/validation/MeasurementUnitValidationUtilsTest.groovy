1bf9bb54fbb281dbd12a0e15d8b63621fed5bb15
Niklas Steffan <niklas.steffan@tu-dortmund.de>
2020-11-18 13:17:07 +0100

    Added MeasurementUnitValidationUtilsTest

---
+++ src/test/groovy/edu/ie3/datamodel/utils/validation/MeasurementUnitValidationUtilsTest.groovy
@@ -0,0 +1,38 @@
+package edu.ie3.datamodel.utils.validation
+
+import edu.ie3.datamodel.exceptions.InvalidEntityException
+import edu.ie3.datamodel.exceptions.UnsafeEntityException
+import edu.ie3.test.common.GridTestData
+import spock.lang.Specification
+
+class MeasurementUnitValidationUtilsTest extends Specification {
+
+    def "Smoke Test: Correct measurement unit throws no exception"() {
+        given:
+        def measurementUnitInput = GridTestData.measurementUnitInput
+
+        when:
+        ValidationUtils.check(measurementUnitInput)
+
+        then:
+        noExceptionThrown()
+    }
+
+    def "MeasurementUnitValidationUtils.check() recognizes all potential errors for a measurement unit"() {
+        when:
+        MeasurementUnitValidationUtils.check(invalidMeasurementUnit)
+
+        then:
+        Exception ex = thrown()
+        ex.class == expectedException.class
+        ex.message == expectedException.message
+
+        where:
+        invalidMeasurementUnit                                                                     || expectedException
+        // GridTestData.measurementUnitInput.copy().node(null).build()                                || new InvalidEntityException("Node of measurement unit is null", invalidMeasurementUnit)
+        GridTestData.measurementUnitInput.copy().vMag(false).vAng(false).p(false).q(false).build() || new UnsafeEntityException("Measurement Unit does not measure any values", invalidMeasurementUnit)
+    }
+    // TODO NSteffan: if node is null, causes NullPointerException in toString function,
+    //  can't create string for UnsafeEntityException
+
+}
