7560e7bf17e125e6f49a6bf74a4ef193e1f52ef1
ahamshubham <ahamshubham@gmail.com>
2020-11-30 02:08:09 +0100

    #234 Added deprecated tags and annotations to TarballUtils and its functions

--- src/main/java/edu/ie3/datamodel/io/TarballUtils.java
+++ src/main/java/edu/ie3/datamodel/io/TarballUtils.java
@@ -20,12 +20,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A utility class to compress and decompress .tar.gz archives. I is mainly inspired by the linked
- * MyKong Tutorial
+ * A utility class to compress and decompress .tar.gz archives.
  *
- * @see <a href="https://mkyong.com/java/how-to-create-tar-gz-in-java/">MyKong Tutorial</a>
+ * @deprecated replaced by linked class FileIOUtils.
+ *
+ * @see <a href="https://github.com/ie3-institute/PowerSystemUtils/blob/master/src/main/java/edu/ie3/util/io/FileIOUtils.java">FileIOUtils</a>
  */
-public class TarballUtils {
+@Deprecated public class TarballUtils {
   private static final Logger logger = LoggerFactory.getLogger(TarballUtils.class);
 
   private static final int MAX_AMOUNT_OF_ENTRIES = 5000;
@@ -36,7 +37,17 @@ public class TarballUtils {
     throw new IllegalStateException("This is an Utility Class and not meant to be instantiated");
   }
 
-  public static void compress(Path source, Path archiveFile) throws FileException {
+  /**
+   * Compresses files to .tar.gz format
+   * @param source source path to compress
+   * @param archiveFile path of the target archive file
+   * @throws FileException If unable to create the file
+   * @deprecated replaced by #compress(String, String, boolean) in linked class FileIOUtils. The file paths are now passed
+   * as Strings and there is a need to mention whether a directory has to be compressed or a single file.
+   *
+   * @see <a href="https://github.com/ie3-institute/PowerSystemUtils/blob/master/src/main/java/edu/ie3/util/io/FileIOUtils.java">FileIOUtils</a>
+   */
+  @Deprecated public static void compress(Path source, Path archiveFile) throws FileException {
     Path validatedArchiveFile = checkAndCreateArchive(archiveFile);
 
     /* Open a stream and add content to the archive */
@@ -119,8 +130,11 @@ public class TarballUtils {
    * @return Path to the actual folder, where the content is extracted to
    * @throws FileException If the archive is not in a well shape, the target folder doesn't meet the
    *     requirements or the archive tries to impose harm by exploiting zip slip vulnerability
+   * @deprecated replaced by #extract(Path, Path) in linked class FileIOUtils. The override parameter is invalid now.
+   *
+   * @see <a href="https://github.com/ie3-institute/PowerSystemUtils/blob/master/src/main/java/edu/ie3/util/io/FileIOUtils.java">FileIOUtils</a>
    */
-  public static Path extract(Path archive, Path target, boolean override) throws FileException {
+  @Deprecated public static Path extract(Path archive, Path target, boolean override) throws FileException {
     /* Pre-flight checks and assembly of the target path */
     Path targetDirectory = determineTargetDirectory(archive, target, override);
 
