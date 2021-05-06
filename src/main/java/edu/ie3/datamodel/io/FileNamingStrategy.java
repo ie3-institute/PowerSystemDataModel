4894f85f37a1cc06a010f5f96eba950b4b1f7fbb
Niklas Steffan <niklas.steffan@tu-dortmund.de>
2021-05-06 18:13:53 +0200

    first test working

--- src/main/java/edu/ie3/datamodel/io/FileNamingStrategy.java
+++ src/main/java/edu/ie3/datamodel/io/FileNamingStrategy.java
@@ -1,12 +1,13 @@
 package edu.ie3.datamodel.io;
 
-import edu.ie3.datamodel.io.csv.DefaultDirectoryHierarchy;
 import edu.ie3.datamodel.io.csv.DirectoryNamingStrategy;
 import edu.ie3.datamodel.io.csv.FlatDirectoryHierarchy;
 import edu.ie3.datamodel.io.naming.EntityPersistenceNamingStrategy;
 import edu.ie3.datamodel.models.UniqueEntity;
 import edu.ie3.datamodel.models.timeseries.TimeSeries;
 import edu.ie3.datamodel.models.timeseries.TimeSeriesEntry;
+import edu.ie3.datamodel.models.timeseries.individual.IndividualTimeSeries;
+import edu.ie3.datamodel.models.timeseries.repetitive.LoadProfileInput;
 import edu.ie3.datamodel.models.value.Value;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.logging.log4j.LogManager;
@@ -14,6 +15,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 public class FileNamingStrategy {
 
@@ -28,6 +30,8 @@ public class FileNamingStrategy {
   private final DirectoryNamingStrategy directoryNamingStrategy;
   private final String fileExtension;
 
+  private final String defaultFileExtension = ".csv";
+
 
   public FileNamingStrategy(EntityPersistenceNamingStrategy entityPersistenceNamingStrategy, DirectoryNamingStrategy directoryNamingStrategy, String fileExtension) {
     this.entityPersistenceNamingStrategy = entityPersistenceNamingStrategy;
@@ -35,10 +39,22 @@ public class FileNamingStrategy {
     this.fileExtension = fileExtension;
   }
 
+  public FileNamingStrategy(EntityPersistenceNamingStrategy entityPersistenceNamingStrategy, DirectoryNamingStrategy directoryNamingStrategy) {
+    this.entityPersistenceNamingStrategy = entityPersistenceNamingStrategy;
+    this.directoryNamingStrategy = directoryNamingStrategy;
+    this.fileExtension = defaultFileExtension;
+  }
+
   public FileNamingStrategy(EntityPersistenceNamingStrategy entityPersistenceNamingStrategy) {
     this.entityPersistenceNamingStrategy = entityPersistenceNamingStrategy;
     this.directoryNamingStrategy = new FlatDirectoryHierarchy();
-    this.fileExtension = ".csv";
+    this.fileExtension = defaultFileExtension;
+  }
+
+  public FileNamingStrategy(EntityPersistenceNamingStrategy entityPersistenceNamingStrategy, String fileExtension) {
+    this.entityPersistenceNamingStrategy = entityPersistenceNamingStrategy;
+    this.directoryNamingStrategy = new FlatDirectoryHierarchy();
+    this.fileExtension = fileExtension;
   }
 
 
@@ -86,8 +102,8 @@ public class FileNamingStrategy {
   private Optional<String> getFilePath(String fileName, String subDirectories) {
     if (fileName.isEmpty()) return Optional.empty();
     if (!subDirectories.isEmpty())
-      return Optional.of(FilenameUtils.concat(subDirectories, fileName));
-    else return Optional.of(fileName);
+      return Optional.of(FilenameUtils.concat(subDirectories, fileName).concat(fileExtension));
+    else return Optional.of(fileName.concat(fileExtension));
   }
 
 
@@ -134,6 +150,23 @@ public class FileNamingStrategy {
                       .replaceAll(FILE_SEPARATOR_REGEX, FILE_SEPARATOR_REPLACEMENT);
       return Optional.of(directoryPath);
     }
+  }
+
+
+  public Pattern getIndividualTimeSeriesPattern() {
+    String subDirectory = directoryNamingStrategy.getSubDirectory(IndividualTimeSeries.class).orElse("");
+    return subDirectory.isEmpty()
+            ? entityPersistenceNamingStrategy.getIndividualTimeSeriesPattern()
+            : Pattern.compile(
+            FilenameUtils.concat(subDirectory, entityPersistenceNamingStrategy.getIndividualTimeSeriesPattern().pattern()));
+  }
+
+  public Pattern getLoadProfileTimeSeriesPattern() {
+    String subDirectory = directoryNamingStrategy.getSubDirectory(LoadProfileInput.class).orElse("");
+    return subDirectory.isEmpty()
+            ? entityPersistenceNamingStrategy.getLoadProfileTimeSeriesPattern()
+            : Pattern.compile(
+            FilenameUtils.concat(subDirectory, entityPersistenceNamingStrategy.getLoadProfileTimeSeriesPattern().pattern()));
   }
 
 }
--- src/test/groovy/edu/ie3/datamodel/io/FileNamingStrategyTest.groovy
+++ src/test/groovy/edu/ie3/datamodel/io/FileNamingStrategyTest.groovy
@@ -1,39 +1,45 @@
 package edu.ie3.datamodel.io
 
 import edu.ie3.datamodel.io.csv.DefaultDirectoryHierarchy
-import edu.ie3.datamodel.io.csv.FlatDirectoryHierarchy
 import edu.ie3.datamodel.io.naming.EntityPersistenceNamingStrategy
 import edu.ie3.datamodel.models.result.system.LoadResult
 import spock.lang.Specification
-
-import java.nio.file.Paths
 
 class FileNamingStrategyTest extends Specification {
 
     def "The FileNamingStrategy ..."() {
         given:
-        def ens = new EntityPersistenceNamingStrategy()
-        def dns = new DefaultDirectoryHierarchy("/foo/test/", "testGrid")
-        def fns = new FileNamingStrategy(ens, dns, ".csv")
-        def path = Paths.get("/bla/foo/")
+        def ens = new EntityPersistenceNamingStrategy("prefix", "suffix")
+        def dns = new DefaultDirectoryHierarchy("/foo/test/", "test_grid")
+        def fns = new FileNamingStrategy(ens, dns, ".TXT")
 
         when:
-        def filePath = fns.getFilePath(LoadResult)
+        def filePath = fns.getFilePath(modelClass)
 
         then:
-        filePath == "/bla/foo/xyz.csv"
+        filePath.present
+        filePath.get() == expectedString
+
+        where:
+        modelClass               || expectedString
+        LoadResult               || "test_grid" + File.separator + "results" + File.separator + "participants" + File.separator + "prefix_load_res_suffix" + ".TXT"
     }
 
     def "The FileNamingStrategy ... flat"() {
         given:
-        def ens = new EntityPersistenceNamingStrategy()
+        def ens = new EntityPersistenceNamingStrategy("prefix", "suffix")
         def fns = new FileNamingStrategy(ens)
 
         when:
         def filePath = fns.getFilePath(LoadResult)
 
         then:
-        filePath == "/bla/foo/xyz.csv"
+        filePath.present
+        filePath.get() == expectedString
+
+        where:
+        modelClass               || expectedString
+        LoadResult               || "prefix_load_res_suffix" + ".csv"
     }
 
 
