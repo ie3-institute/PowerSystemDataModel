4222d3903e00d36775ac806269b01de1a4937046
Thomas <thomas.oberliessen@googlemail.com>
2022-06-03 16:21:08 +0200

    Revert "add specific parse methods"
    This reverts commit f47c83e10e0dee59ec79eb3601ff2eabd84d4d15.

--- src/main/java/edu/ie3/datamodel/models/profile/TemperatureDependantLoadProfile.java
+++ src/main/java/edu/ie3/datamodel/models/profile/TemperatureDependantLoadProfile.java
@@ -5,17 +5,9 @@
 */
 package edu.ie3.datamodel.models.profile;
 
-import edu.ie3.datamodel.exceptions.ParsingException;
-
 /**
  * Temperature dependant load profiles for night storage heating and heat pumps . The profiles rely
  * on the VDN description for interruptable loads. For more details see <a
  * href="https://www.bdew.de/media/documents/LPuVe-Praxisleitfaden.pdf">here</a>.
  */
-public interface TemperatureDependantLoadProfile extends LoadProfile {
-
-  static NbwTemperatureDependantLoadProfile parse(String key) throws ParsingException {
-    return (NbwTemperatureDependantLoadProfile)
-        LoadProfile.parse(NbwTemperatureDependantLoadProfile.values(), key);
-  }
-}
+public interface TemperatureDependantLoadProfile extends LoadProfile {}
