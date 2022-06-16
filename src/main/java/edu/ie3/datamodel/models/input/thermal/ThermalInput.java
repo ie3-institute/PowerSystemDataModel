257fd71737d9661e7bb586384ae22da7798f49ae
Kittl, Chris <chris.kittl@tu-dortmund.de>
2022-06-16 13:38:13 +0200

    Add distinct thermal input abstract class

---
+++ src/main/java/edu/ie3/datamodel/models/input/thermal/ThermalInput.java
@@ -0,0 +1,22 @@
+/*
+ * © 2022. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+*/
+package edu.ie3.datamodel.models.input.thermal;
+
+import edu.ie3.datamodel.models.OperationTime;
+import edu.ie3.datamodel.models.input.AssetInput;
+import edu.ie3.datamodel.models.input.OperatorInput;
+import java.util.UUID;
+
+public abstract class ThermalInput extends AssetInput {
+  protected ThermalInput(
+      UUID uuid, String id, OperatorInput operator, OperationTime operationTime) {
+    super(uuid, id, operator, operationTime);
+  }
+
+  public ThermalInput(UUID uuid, String id) {
+    super(uuid, id);
+  }
+}
