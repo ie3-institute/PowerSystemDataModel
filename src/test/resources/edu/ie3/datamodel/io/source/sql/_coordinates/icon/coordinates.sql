00c21f277b48816ee2dd5215a373df75b56965eb
Marius Staudt <marius.staudt@tu-dortmund.de>
2022-10-31 13:23:09 +0100

    Adding missing file for SqlIdCoordinateSource test.

---
+++ src/test/resources/edu/ie3/datamodel/io/source/sql/_coordinates/icon/coordinates.sql
@@ -0,0 +1,22 @@
+CREATE TABLE public.coordinates(
+
+    id,
+    latitude double precision,
+    longitude double precision,
+    coordinate_type
+)
+    WITH (
+        OIDS = FALSE
+    )
+    TABLESPACE pg_default;
+
+
+
+
+INSERT INTO
+    public.coordinates(id, latitude, longitude, coordinate_type)
+VALUES
+(67775,51.5,7.438,ICON),
+(531137,51.5,7.375,ICON),
+(551525,51.438,7.438,ICON),
+(278150,51.438,7.375,ICON)
