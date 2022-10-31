9b59372351d10b7208806c16844ba5c4a2f5ae29
Marius Staudt <marius.staudt@tu-dortmund.de>
2022-10-31 13:57:06 +0100

    fmt

--- src/test/resources/edu/ie3/datamodel/io/source/sql/_coordinates/icon/coordinates.sql
+++ src/test/resources/edu/ie3/datamodel/io/source/sql/_coordinates/icon/coordinates.sql
@@ -12,7 +12,7 @@ CREATE TABLE public.coordinates
 INSERT INTO
     public.coordinates(id, latitude, longitude, coordinate_type)
 VALUES
-(67775,51.5,7.438,ICON),
-(531137,51.5,7.375,ICON),
-(551525,51.438,7.438,ICON),
-(278150,51.438,7.375,ICON)
+(67775,51.5,7.438,"ICON"),
+(531137,51.5,7.375,"ICON"),
+(551525,51.438,7.438,"ICON"),
+(278150,51.438,7.375,"ICON")
