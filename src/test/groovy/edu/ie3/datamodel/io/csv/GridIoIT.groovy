0027e04652ce7bdb1b624bed32c9f8a2c78cd1dc
Vicky Bung <vicky.bung@tu-dortmund.de>
2023-02-14 13:23:11 +0100

    Moved package

--- src/test/groovy/edu/ie3/datamodel/io/csv/GridIoIT.groovy
+++ src/test/groovy/edu/ie3/datamodel/io/csv/GridIoIT.groovy
@@ -3,7 +3,7 @@
  * Institute of Energy Systems, Energy Efficiency and Energy Economics,
  * Research group Distribution grid planning and operation
  */
-package edu.ie3.datamodel.io
+package edu.ie3.datamodel.io.csv
 
 import edu.ie3.datamodel.io.sink.CsvFileSink
 import edu.ie3.datamodel.io.source.csv.CsvJointGridContainerSource
