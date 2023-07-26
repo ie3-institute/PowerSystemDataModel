14e458a108657b984a68d38c00e572179a52a085
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2023-07-26 11:45:03 +0200

    .

--- src/main/java/edu/ie3/datamodel/utils/Try.java
+++ src/main/java/edu/ie3/datamodel/utils/Try.java
@@ -252,8 +252,7 @@ public abstract class Try<T, E extends Exception> {
     List<Try<U, E>> failures = map.get(false);
 
     // Both lists should exist in map per definition of partitioningBy
-    assert successes != null;
-    assert failures != null;
+    assert successes != null && failures != null;
 
     if (!failures.isEmpty()) {
       E first = failures.get(0).exception;
