673e9d3fe2da6d1b1b46f387edb53fc2dc99cd83
staudtMarius <marius.staudt@tu-dortmund.de>
2023-07-28 16:52:47 +0200

    Fixing ``codacy`` issue.

--- src/test/groovy/edu/ie3/datamodel/io/source/csv/CsvRawGridSourceTest.groovy
+++ src/test/groovy/edu/ie3/datamodel/io/source/csv/CsvRawGridSourceTest.groovy
@@ -19,7 +19,6 @@ import edu.ie3.datamodel.models.input.container.RawGridElements
 import edu.ie3.datamodel.utils.Try
 import edu.ie3.test.common.GridTestData
 import edu.ie3.test.common.GridTestData as rgtd
-import org.junit.runner.notification.Failure
 import spock.lang.Shared
 import spock.lang.Specification
 
