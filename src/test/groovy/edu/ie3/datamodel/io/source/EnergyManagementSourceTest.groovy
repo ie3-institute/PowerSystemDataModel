136132bc0464fefb216c910ac50d5bafcdac5e2e
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2024-01-03 13:47:10 +0100

    Ok, try again

--- src/test/groovy/edu/ie3/datamodel/io/source/EnergyManagementSourceTest.groovy
+++ src/test/groovy/edu/ie3/datamodel/io/source/EnergyManagementSourceTest.groovy
@@ -225,7 +225,7 @@ class EnergyManagementSourceTest extends Specification {
     then:
     def exc = thrown(SourceException)
     exc.cause.message.contains("An error occurred when creating instance of EmInput")
-    exc.cause.class == FactoryException
+    exc.cause.cause.class == FactoryException
   }
 
   def "An EnergyManagementSource should fail if a parent em is not provided"() {
