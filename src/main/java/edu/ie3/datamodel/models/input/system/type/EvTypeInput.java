9371a4b2e268fa07ec633fa46d0344aa1784905d
pierrepetersmeier <155652256+pierrepetersmeier@users.noreply.github.com>
2024-02-08 16:11:51 +0100

    -Updated SystemParticipantValidationUtilsTest.groovy -correct my mistakes kw=kva

--- src/main/java/edu/ie3/datamodel/models/input/system/type/EvTypeInput.java
+++ src/main/java/edu/ie3/datamodel/models/input/system/type/EvTypeInput.java
@@ -33,10 +33,10 @@ public class EvTypeInput extends SystemParticipantTypeInput {
    * @param opex Operating expense for this type of EV (typically in €)
    * @param eStorage Energy capacity of the storage
    * @param eCons Consumed electric energy per driven distance
-   * @param sRated Rated apparent power for this type of EV (typically in kVA)
+   * @param sRated Rated apparent power for this type of EV (typically in kW)
    * @param cosphiRated Power factor for this type of EV
-   * @param sRatedAC Rated apparent power for AC charging for this type of EV (typically in kVA)
-   * @param sRatedDC Rated apparent power for DC charging for this type of EV (typically in kVA)
+   * @param sRatedAC Rated apparent power for AC charging for this type of EV (typically in kW)
+   * @param sRatedDC Rated apparent power for DC charging for this type of EV (typically in kW)
    */
   public EvTypeInput(
       UUID uuid,
