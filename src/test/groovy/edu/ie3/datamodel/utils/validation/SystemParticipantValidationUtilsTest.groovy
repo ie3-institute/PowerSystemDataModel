d96e4991b25ed88a5f8e0d971306603e31e7760c
pierrepetersmeier <155652256+pierrepetersmeier@users.noreply.github.com>
2024-02-08 16:20:23 +0100

    -Space after comma

--- src/test/groovy/edu/ie3/datamodel/utils/validation/SystemParticipantValidationUtilsTest.groovy
+++ src/test/groovy/edu/ie3/datamodel/utils/validation/SystemParticipantValidationUtilsTest.groovy
@@ -239,7 +239,7 @@ class SystemParticipantValidationUtilsTest extends Specification {
 
     where:
     invalidEvType                                                                                                                                     || expectedException
-    new EvTypeInput(uuid, id, capex, opex, Quantities.getQuantity(0, ENERGY_IN), Quantities.getQuantity(0, ENERGY_PER_DISTANCE), sRated, cosPhiRated, Quantities.getQuantity(20,S_RATED ), Quantities.getQuantity(20,S_RATED )) || new InvalidEntityException("The following quantities have to be positive: 0 kWh, 0 kWh/km", invalidEvType)
+    new EvTypeInput(uuid, id, capex, opex, Quantities.getQuantity(0, ENERGY_IN), Quantities.getQuantity(0, ENERGY_PER_DISTANCE), sRated, cosPhiRated, Quantities.getQuantity(20, S_RATED), Quantities.getQuantity(20, S_RATED)) || new InvalidEntityException("The following quantities have to be positive: 0 kWh, 0 kWh/km", invalidEvType)
   }
 
   // Fixed Feed In
