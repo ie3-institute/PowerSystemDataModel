b975695ef624c20d0aa19ed7cd32640f6cb1cc27
danielfeismann <daniel.feismann@tu-dortmund.de>
2024-02-28 09:39:44 +0100

    revert Default_Timestamp_Pattern

--- src/main/java/edu/ie3/datamodel/io/source/couchbase/CouchbaseWeatherSource.java
+++ src/main/java/edu/ie3/datamodel/io/source/couchbase/CouchbaseWeatherSource.java
@@ -34,9 +34,8 @@ import org.slf4j.LoggerFactory;
 /** Couchbase Source for weather data */
 public class CouchbaseWeatherSource extends WeatherSource {
   private static final Logger logger = LoggerFactory.getLogger(CouchbaseWeatherSource.class);
-  /** The start of the document key, comparable to a table name in relational databases */
   private static final String DEFAULT_TIMESTAMP_PATTERN = "yyyy-MM-dd'T'HH:mm:ssxxx";
-
+  /** The start of the document key, comparable to a table name in relational databases */
   private static final String DEFAULT_KEY_PREFIX = "weather";
 
   private final String keyPrefix;
