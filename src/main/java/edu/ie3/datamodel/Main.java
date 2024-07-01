d77e478f2bb81212eae906dc5dd7e23d6c879ebb
Johannes Bao <johannes.bao@tu-dortmund.de>
2024-07-01 08:07:35 +0200

    Main Method

---
+++ src/main/java/edu/ie3/datamodel/Main.java
@@ -0,0 +1,21 @@
+package edu.ie3.datamodel;
+
+import edu.ie3.datamodel.exceptions.EntityProcessorException;
+import edu.ie3.datamodel.exceptions.ProcessorProviderException;
+import edu.ie3.datamodel.io.SqlUtils;
+import edu.ie3.datamodel.models.input.system.type.StorageTypeInput;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println(SqlUtils.queryCreateTableUniqueEntity(
+                    StorageTypeInput.class,
+                    "public"
+            ));
+        } catch (EntityProcessorException e) {
+            throw new RuntimeException(e);
+        } catch (ProcessorProviderException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
