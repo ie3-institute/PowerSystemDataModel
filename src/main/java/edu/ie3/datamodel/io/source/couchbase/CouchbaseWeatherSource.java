fd0823be8fbe9680498f7e10cb745ecf5a3a0528
Philipp Schmelter <philipp@stressnet.de>
2025-08-21 02:06:12 +0200

    fx

--- src/main/java/edu/ie3/datamodel/io/source/couchbase/CouchbaseWeatherSource.java
+++ src/main/java/edu/ie3/datamodel/io/source/couchbase/CouchbaseWeatherSource.java
@@ -128,7 +128,9 @@ public class CouchbaseWeatherSource extends WeatherSource {
         try {
           jsonWeatherInputs = queryResult.rowsAsObject();
         } catch (DecodingFailureException ex) {
-          logger.error("Querying weather inputs failed!", ex);
+          logger.error("Querying weather inputs failed for coordinate {}", coordinate, ex);
+          throw new NoDataException(
+              "Failed to decode weather data for coordinate " + coordinate, ex);
         }
         if (jsonWeatherInputs != null && !jsonWeatherInputs.isEmpty()) {
           Set<TimeBasedValue<WeatherValue>> weatherInputs =
