48568d8899ed9b2cd1c00f6a6afa380189301e0a
kkarthik11 <58599230+kkarthik11@users.noreply.github.com>
2025-09-15 10:57:36 +0200

    Update WecInput.java
    changes Wecinput #1412

--- src/main/java/edu/ie3/datamodel/models/input/system/WecInput.java
+++ src/main/java/edu/ie3/datamodel/models/input/system/WecInput.java
@@ -13,6 +13,7 @@ import edu.ie3.datamodel.models.input.OperatorInput;
 import edu.ie3.datamodel.models.input.system.characteristic.ReactivePowerCharacteristic;
 import edu.ie3.datamodel.models.input.system.type.WecTypeInput;
 import java.util.Objects;
+import javax.measure.quantity.Power;
 import java.util.UUID;
 
 /** Describes a Wind Energy Converter */
