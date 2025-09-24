84741d95ac04ca408681ea6f513c870c262ffe58
kkarthik11 <58599230+kkarthik11@users.noreply.github.com>
2025-09-24 21:24:58 +0200

    Update EvcsInputTest.groovy
    updated sRated error for evcsInput groovy #1412

--- src/test/groovy/edu/ie3/datamodel/models/input/system/EvcsInputTest.groovy
+++ src/test/groovy/edu/ie3/datamodel/models/input/system/EvcsInputTest.groovy
@@ -55,7 +55,7 @@ class EvcsInputTest extends Specification {
       assert id == evcsInput.id
       assert qCharacteristics == evcsInput.qCharacteristics
       assert type.sRated == evcsInput.type.sRated * 2d
-      assert sRated() == hpInput.type.sRated * 2d
+      assert sRated() == evcsInput.type.sRated * 2d
       assert cosPhiRated == evcsInput.cosPhiRated
       assert chargingPoints == evcsInput.chargingPoints
       assert locationType == evcsInput.locationType
