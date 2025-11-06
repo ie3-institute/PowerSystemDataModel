a1967ea8cb96caa4f75f7900e82d509d60b57936
Pierre <155652256+pierrepetersmeier@users.noreply.github.com>
2025-11-06 13:15:00 +0100

    Rename ground temperature columns in SQL schema

--- src/test/resources/edu/ie3/datamodel/io/sink/_sql/time_series.sql
+++ src/test/resources/edu/ie3/datamodel/io/sink/_sql/time_series.sql
@@ -99,8 +99,8 @@ CREATE TABLE public.time_series_weather
     direction DOUBLE PRECISION,
     temperature DOUBLE PRECISION,
     velocity DOUBLE PRECISION,
-    ground_temperature_value_one DOUBLE PRECISION,
-    ground_temperature_value_two DOUBLE PRECISION,
+    ground_temperature_level_1 DOUBLE PRECISION,
+    ground_temperature_level_2 DOUBLE PRECISION,
     grid_uuid UUID NOT NULL REFERENCES grids(uuid)
 )
     WITHOUT OIDS
