fbf01ee3b07e7deda2fb1dd08c74bd3102d240e2
Philipp <philipp@stressnet.de>
2025-12-11 02:28:31 +0100

    spotless

--- src/main/java/edu/ie3/datamodel/io/factory/markov/MarkovModelParsingSupport.java
+++ src/main/java/edu/ie3/datamodel/io/factory/markov/MarkovModelParsingSupport.java
@@ -1,8 +1,8 @@
 /*
- * ЖИ 2025. TU Dortmund University,
+ * © 2025. TU Dortmund University,
  * Institute of Energy Systems, Energy Efficiency and Energy Economics,
  * Research group Distribution grid planning and operation
- */
+*/
 package edu.ie3.datamodel.io.factory.markov;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -214,8 +214,7 @@ interface MarkovModelParsingSupport {
     }
   }
 
-  default double[][][] parseTransitionValues(
-      JsonNode valuesNode, int buckets, int stateCount) {
+  default double[][][] parseTransitionValues(JsonNode valuesNode, int buckets, int stateCount) {
     if (!valuesNode.isArray()) {
       throw new FactoryException("Transition values must be a three dimensional array");
     }
