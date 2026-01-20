22c936aba4bad0ad9f602705d29e5935c10e2a46
pierrepetersmeier <155652256+pierrepetersmeier@users.noreply.github.com>
2026-01-20 10:15:53 +0100

    Convert ground temperature values to standard units in IconWeatherTestData

--- src/test/groovy/edu/ie3/test/common/IconWeatherTestData.groovy
+++ src/test/groovy/edu/ie3/test/common/IconWeatherTestData.groovy
@@ -9,6 +9,7 @@ import edu.ie3.datamodel.models.StandardUnits
 import edu.ie3.datamodel.models.value.WeatherValue
 import edu.ie3.util.TimeUtil
 import tech.units.indriya.quantity.Quantities
+import tech.units.indriya.unit.Units
 
 import java.time.ZonedDateTime
 
@@ -24,8 +25,8 @@ class IconWeatherTestData extends WeatherTestData {
   Quantities.getQuantity(24.4741992659816, StandardUnits.TEMPERATURE),
   Quantities.getQuantity(270.45278309919627, StandardUnits.WIND_DIRECTION),
   Quantities.getQuantity(3.76601470961371, StandardUnits.WIND_VELOCITY),
-  Optional.of(Quantities.getQuantity(100.123456789123, StandardUnits.TEMPERATURE)),
-  Optional.of(Quantities.getQuantity(200.123456789123, StandardUnits.TEMPERATURE))
+  Optional.of(Quantities.getQuantity(100.123456789123, Units.KELVIN).to(StandardUnits.TEMPERATURE)),
+  Optional.of(Quantities.getQuantity(200.123456789123, Units.KELVIN).to(StandardUnits.TEMPERATURE))
   )
 
   public static final WeatherValue WEATHER_VALUE_67775_16H = new WeatherValue(
@@ -35,8 +36,8 @@ class IconWeatherTestData extends WeatherTestData {
   Quantities.getQuantity(24.1700023473353, StandardUnits.TEMPERATURE),
   Quantities.getQuantity(278.144331776102, StandardUnits.WIND_DIRECTION),
   Quantities.getQuantity(4.05744164637287, StandardUnits.WIND_VELOCITY),
-  Optional.of(Quantities.getQuantity(300.123456789123, StandardUnits.TEMPERATURE)),
-  Optional.of(Quantities.getQuantity(400.123456789123, StandardUnits.TEMPERATURE))
+  Optional.of(Quantities.getQuantity(300.123456789123, Units.KELVIN).to(StandardUnits.TEMPERATURE)),
+  Optional.of(Quantities.getQuantity(400.123456789123, Units.KELVIN).to(StandardUnits.TEMPERATURE))
   )
 
   public static final WeatherValue WEATHER_VALUE_67775_17H = new WeatherValue(
@@ -46,8 +47,8 @@ class IconWeatherTestData extends WeatherTestData {
   Quantities.getQuantity(23.6787403584074, StandardUnits.TEMPERATURE),
   Quantities.getQuantity(286.891007103442, StandardUnits.WIND_DIRECTION),
   Quantities.getQuantity(3.81526300455393, StandardUnits.WIND_VELOCITY),
-  Optional.of(Quantities.getQuantity(500.123456789123, StandardUnits.TEMPERATURE)),
-  Optional.of(Quantities.getQuantity(600.123456789123, StandardUnits.TEMPERATURE))
+  Optional.of(Quantities.getQuantity(500.123456789123, Units.KELVIN).to(StandardUnits.TEMPERATURE)),
+  Optional.of(Quantities.getQuantity(600.123456789123, Units.KELVIN).to(StandardUnits.TEMPERATURE))
   )
 
   public static final WeatherValue WEATHER_VALUE_67776_15H = new WeatherValue(
@@ -57,8 +58,8 @@ class IconWeatherTestData extends WeatherTestData {
   Quantities.getQuantity(22.365335568404, StandardUnits.TEMPERATURE),
   Quantities.getQuantity(245.604554131632, StandardUnits.WIND_DIRECTION),
   Quantities.getQuantity(4.39390441381814, StandardUnits.WIND_VELOCITY),
-  Optional.of(Quantities.getQuantity(700.123456789123, StandardUnits.TEMPERATURE)),
-  Optional.of(Quantities.getQuantity(800.123456789123, StandardUnits.TEMPERATURE))
+  Optional.of(Quantities.getQuantity(700.123456789123, Units.KELVIN).to(StandardUnits.TEMPERATURE)),
+  Optional.of(Quantities.getQuantity(800.123456789123, Units.KELVIN).to(StandardUnits.TEMPERATURE))
   )
 
   public static final WeatherValue WEATHER_VALUE_67776_16H = new WeatherValue(
@@ -68,7 +69,7 @@ class IconWeatherTestData extends WeatherTestData {
   Quantities.getQuantity(20.305111314491, StandardUnits.TEMPERATURE),
   Quantities.getQuantity(252.810224701109, StandardUnits.WIND_DIRECTION),
   Quantities.getQuantity(3.44242472583919, StandardUnits.WIND_VELOCITY),
-  Optional.of(Quantities.getQuantity(900.123456789123, StandardUnits.TEMPERATURE)),
-  Optional.of(Quantities.getQuantity(1000.123456789123, StandardUnits.TEMPERATURE))
+  Optional.of(Quantities.getQuantity(900.123456789123, Units.KELVIN).to(StandardUnits.TEMPERATURE)),
+  Optional.of(Quantities.getQuantity(1000.123456789123, Units.KELVIN).to(StandardUnits.TEMPERATURE))
   )
 }
