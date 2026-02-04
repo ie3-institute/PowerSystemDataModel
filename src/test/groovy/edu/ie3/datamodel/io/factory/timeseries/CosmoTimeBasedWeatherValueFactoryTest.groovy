f97f60102dc732f5b13ced7925cf173b876f1df3
pierrepetersmeier <155652256+pierrepetersmeier@users.noreply.github.com>
2026-02-04 12:37:41 +0100

    Refactor tests for Cosmo and Icon time-based weather value factories to verify handling of missing temperature values. Update assertions to check for expected results instead of throwing exceptions.

--- src/test/groovy/edu/ie3/datamodel/io/factory/timeseries/CosmoTimeBasedWeatherValueFactoryTest.groovy
+++ src/test/groovy/edu/ie3/datamodel/io/factory/timeseries/CosmoTimeBasedWeatherValueFactoryTest.groovy
@@ -17,31 +17,41 @@ import tech.units.indriya.unit.Units
 
 class CosmoTimeBasedWeatherValueFactoryTest extends Specification {
 
-  def "A PsdmTimeBasedWeatherValueFactory should throw an Exception if a required field is empty"() {
-    given:
-    def factory = new CosmoTimeBasedWeatherValueFactory()
-    def coordinate = CosmoWeatherTestData.COORDINATE_193186
+    def "A PsdmTimeBasedWeatherValueFactory should be able to create time series with missing values"() {
+        given:
+        def factory = new CosmoTimeBasedWeatherValueFactory()
+        def coordinate = CosmoWeatherTestData.COORDINATE_193186
+        def time = TimeUtil.withDefaults.toZonedDateTime("2019-01-01T00:00:00Z")
 
-    Map<String, String> parameter = [
-      "time"                    : "2019-01-01T00:00:00Z",
-      "diffuseIrradiance"       : "282.671997070312",
-      "directIrradiance"        : "286.872985839844",
-      "temperature"             : "",
-      "windDirection"           : "0",
-      "windVelocity"            : "1.66103506088257",
-      "groundTemperatureLevel1" : "",
-      "groundTemperatureLevel2" : ""
-    ]
+        Map<String, String> parameter = [
+                "time"                    : TimeUtil.withDefaults.toString(time),
+                "diffuseIrradiance"       : "282.671997070312",
+                "directIrradiance"        : "286.872985839844",
+                "temperature"             : "",
+                "windDirection"           : "0",
+                "windVelocity"            : "1.66103506088257",
+                "groundTemperatureLevel1" : "",
+                "groundTemperatureLevel2" : ""
+        ]
 
-    def data = new TimeBasedWeatherValueData(parameter, coordinate)
+        def data = new TimeBasedWeatherValueData(parameter, coordinate)
 
-    when:
-    factory.buildModel(data)
+        def expectedResults = new TimeBasedValue(
+                time, new WeatherValue(coordinate,
+                Quantities.getQuantity(286.872985839844d, StandardUnits.SOLAR_IRRADIANCE),
+                Quantities.getQuantity(282.671997070312d, StandardUnits.SOLAR_IRRADIANCE),
+                null,
+                Quantities.getQuantity(0d, StandardUnits.WIND_DIRECTION),
+                Quantities.getQuantity(1.66103506088257d, StandardUnits.WIND_VELOCITY),
+                Optional.empty(),
+                Optional.empty()))
 
-    then:
-    def exception = thrown(FactoryException)
-    exception.message.toLowerCase().contains("temperature")
-  }
+        when:
+        def model = factory.buildModel(data)
+
+        then:
+        Objects.equals(model,expectedResults)
+    }
 
   def "A PsdmTimeBasedWeatherValueFactory should be able to create time series values"() {
     given:
