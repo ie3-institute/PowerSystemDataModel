18d681b01f226a46de53add25c59bfd68cf29bb1
danielfeismann <daniel.feismann@tu-dortmund.de>
2026-03-25 14:12:43 +0100

    update couchbase and sql dependencies in tests

--- src/test/groovy/edu/ie3/datamodel/io/source/couchbase/CouchbaseWeatherSourceIconIT.groovy
+++ src/test/groovy/edu/ie3/datamodel/io/source/couchbase/CouchbaseWeatherSourceIconIT.groovy
@@ -28,7 +28,7 @@ class CouchbaseWeatherSourceIconIT extends Specification implements TestContaine
   BucketDefinition bucketDefinition = new BucketDefinition("ie3_in")
 
   @Shared
-  CouchbaseContainer couchbaseContainer = new CouchbaseContainer("couchbase/server:6.6.0")
+  CouchbaseContainer couchbaseContainer = new CouchbaseContainer("couchbase/server:8.0.0")
   .withBucket(bucketDefinition)
   .withExposedPorts(8091, 8092, 8093, 8094, 11210)
   .withStartupAttempts(3) // 3 attempts because startup (node renaming) sometimes fails when executed too early
