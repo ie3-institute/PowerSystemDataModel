0d7c64e361eb9975c2e5a46545490c8ff31de193
pierrepetersmeier <155652256+pierrepetersmeier@users.noreply.github.com>
2026-04-30 15:14:45 +0200

    Remove redundant tests for CsvIdCoordinateSource in Icon and Cosmo integration tests

--- src/test/groovy/edu/ie3/datamodel/io/source/csv/CsvIdCoordinateSourceCosmoIT.groovy
+++ src/test/groovy/edu/ie3/datamodel/io/source/csv/CsvIdCoordinateSourceCosmoIT.groovy
@@ -25,21 +25,6 @@ class CsvIdCoordinateSourceCosmoIT extends Specification implements CsvTestDataM
     source = new CsvIdCoordinateSource(new CosmoIdCoordinateFactory(), new CsvDataSource(csvSep, coordinatesCosmoFolderPath, fileNamingStrategy))
   }
 
-  def "The CsvCoordinateSource is able to create a valid stream from a coordinate file"() {
-    def expectedStream = Stream.of(
-        ["id": "106580", "latgeo": "39.602772", "latrot": "-10", "longgeo": "1.279336", "longrot": "-6.8125", "tid": "1"],
-        ["id": "106581", "latgeo": "39.610001", "latrot": "-10", "longgeo": "1.358673", "longrot": "-6.75", "tid": "2"],
-        ["id": "106582", "latgeo": "39.617161", "latrot": "-10", "longgeo": "1.438028", "longrot": "-6.6875", "tid": "3"],
-        ["id": "106583", "latgeo": "39.624249", "latrot": "-10", "longgeo": "1.5174021", "longrot": "-6.625", "tid": "4"])
-
-    when:
-    def actualStream = source.buildStreamWithFieldsToAttributesMap()
-
-    then:
-    actualStream.success
-    actualStream.data.get().collect(Collectors.toList()).containsAll(expectedStream.collect(Collectors.toList()))
-  }
-
   def "The CsvCoordinateSource is able to return the source fields from a coordinate file"() {
     when:
     def sourceFields = source.sourceFields
