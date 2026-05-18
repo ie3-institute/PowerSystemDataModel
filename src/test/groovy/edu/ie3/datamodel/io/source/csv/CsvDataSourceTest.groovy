f6ae4877da0be8842020083b160bde9e70812e25
danielfeismann <daniel.feismann@tu-dortmund.de>
2026-05-18 10:05:14 +0200

    fmt ie3 license header same way for groovy as for java

--- src/test/groovy/edu/ie3/datamodel/io/source/csv/CsvDataSourceTest.groovy
+++ src/test/groovy/edu/ie3/datamodel/io/source/csv/CsvDataSourceTest.groovy
@@ -2,7 +2,7 @@
  * © 2021. TU Dortmund University,
  * Institute of Energy Systems, Energy Efficiency and Energy Economics,
  * Research group Distribution grid planning and operation
- */
+*/
 package edu.ie3.datamodel.io.source.csv
 
 import edu.ie3.datamodel.exceptions.SourceException
--- src/test/groovy/edu/ie3/datamodel/models/input/system/EvcsInputTest.groovy
+++ src/test/groovy/edu/ie3/datamodel/models/input/system/EvcsInputTest.groovy
@@ -2,7 +2,7 @@
  * © 2021. TU Dortmund University,
  * Institute of Energy Systems, Energy Efficiency and Energy Economics,
  * Research group Distribution grid planning and operation
- */
+*/
 package edu.ie3.datamodel.models.input.system
 
 import edu.ie3.datamodel.models.input.system.type.chargingpoint.ChargingPointTypeUtils
